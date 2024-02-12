364afe51a55bca2399b2af6510186032c703c082
何卓霖 <1224430363@qq.com>
2024-02-12 17:26:49 +0800

    WA on F

--- Round922Div2/F.cpp
+++ Round922Div2/F.cpp
@@ -11,19 +11,51 @@ typedef std::pair<ll, int> pli;
 typedef std::pair<ll, ll> pll;
 typedef double db;
 typedef long double ldb;
-const ll mod = 998244353;
 const int maxn = 2e5 + 5;
-const double eps = 1e-10;
-
+int k, n;
+std::vector<int> distanceDiff;
+std::vector<int> edge[maxn];
+int depth[maxn], deepestLeaf[maxn];
+ll total;
+void dfs(int u) {
+    deepestLeaf[u] = depth[u];
+    std::vector<int> vec;
+    for (auto v : edge[u]) {
+        depth[v] = depth[u] + 1;
+        dfs(v);
+        deepestLeaf[u] = std::max(deepestLeaf[u], deepestLeaf[v] + 1);
+        vec.push_back(deepestLeaf[v]);
+    }
+    sort(all(vec));
+    // vec.size()是无符号的，如果vec为空，减1会溢出
+    for (int i = 0 ; i < (int)vec.size() - 1; ++i) {
+        total += vec[i] - depth[u];
+        distanceDiff.push_back(std::max(0, vec[i] - depth[u] * 2));
+    }
+}
+bool cmp(int x, int y) {
+    return x > y;
+}
 int main() {
-    freopen("input.txt", "r", stdin);
-    freopen("output.txt", "w", stdout);
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
     std::ios::sync_with_stdio(false);
     std::cin.tie(NULL);
 
     int T = 1;
-    std::cin >> T;
+    // std::cin >> T;
     while (T--) {
+        std::cin >> n >> k;
+        for (int i = 2; i <= n; ++i) {
+            int j;
+            std::cin >> j;
+            edge[j].push_back(i);
+        }
+        dfs(1);
+        sort(distanceDiff.begin(), distanceDiff.end(), cmp);
+        for (int i = 0; i < std::min(k, (int)distanceDiff.size()); ++i)
+            total -= distanceDiff[i];
+        std::cout << total + n - 1 << '\n';
     }
 
     return 0;
---
+++ Round922Div2/test.cpp
@@ -0,0 +1,27 @@
+#include <algorithm>
+#include <cstdio>
+#include <iostream>
+#include <map>
+#include <set>
+#include <vector>
+#define all(x) x.begin(), x.end()
+typedef int64_t ll;
+typedef std::pair<int, int> pii;
+typedef std::pair<ll, int> pli;
+typedef std::pair<ll, ll> pll;
+typedef double db;
+typedef long double ldb;
+const int maxn = 2e5 + 5;
+
+int main() {
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(NULL);
+
+    for (int i = 1; i < 0; ++i) {
+        std::cout << "WTF!" << '\n';
+    }
+
+    return 0;
+}
